5a6b5e98d2adf7bb9c4d93b2144867b0fd76e83c
Alex Dalton <alex.k.dalton@gmail.com>
2018-01-24 12:01:29 +0000

    actual random sampling

--- evolve.c
+++ evolve.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define MUTATION 0.05
 
 typedef struct {
 	unsigned char values[4];
+	float eval;
 } Individual;
 
 void print_ind( Individual ind )
@@ -18,21 +22,62 @@ void print_ind( Individual ind )
 	}
 }
 
+float evaluate( Individual ind )
+{
+	int score = 0;
+	for ( int i = 0 ; i < 4 ; i++ )
+	{
+		for ( int j = 7 ; j >= 0 ; j-- )
+		{
+			if ( !!(ind.values[ i ] & ( 1 << j )) )
+			{
+				score++;
+			}
+		}
+	}
+
+	return (float)score/32;
+}
+
+void evolve( Individual *pop )
+{
+	Individual most_fit;
+	most_fit.eval = 0;
+
+	for ( int i = 0 ; i < 50 ; i++ )
+	{
+		pop[ i ].eval = evaluate( pop[ i ] );
+
+		if ( most_fit.eval < pop[ i ].eval )
+		{
+			most_fit = pop[ i ];
+		}
+	}
+
+	printf( "Most fit bitstring : " );
+	print_ind( most_fit );
+	printf( " %.3f\n", most_fit.eval );
+}
+
 int main()
 {
 	Individual pop[50];
-	
+	int random;
+
+	int rng = open( "/dev/urandom", O_RDONLY );
+	read( rng, &random, sizeof( int ) );
+	close( rng );
+	srand( random );
+
 	for ( int i = 0 ; i < 50 ; i++ )
 	{
 		pop[ i ].values[ 0 ] = (unsigned char)(rand() % 255);
 		pop[ i ].values[ 1 ] = (unsigned char)(rand() % 255);
 		pop[ i ].values[ 2 ] = (unsigned char)(rand() % 255);
 		pop[ i ].values[ 3 ] = (unsigned char)(rand() % 255);
-
-		printf( "%2d : ", i );
-		print_ind( pop[ i ] );
-		printf( "\n" );
 	}
+
+	evolve( pop );
 
 	return 0;
 }
