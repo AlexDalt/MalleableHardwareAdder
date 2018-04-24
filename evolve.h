b18367c485bfc1701112328a0ed977d7e62200de
Alex Dalton <alex.k.dalton@gmail.com>
2018-04-24 12:24:48 +0100

    somtimes the gui covers information

--- evolve.h
+++ evolve.h
@@ -11,7 +11,7 @@
 #define POP_SIZE 50
 #define MUTATION 2.7f
 #define SIZE_WEIGHT 0
-#define DIVERSITY_WEIGHT 5
+#define DIVERSITY_WEIGHT 4
 #define ELITISM 1
 #define FITNESS_WEIGHT 10
 #define COEVOLVE 0
